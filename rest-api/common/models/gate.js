dc54943ed6e096cab45fa4949cc7469fd35336f7
Eric Bukowski <john.eric.bukowski@gmail.com>
2017-02-10 15:39:29 -0700

    Add remote method `details` to `gate`

--- rest-api/common/models/gate.js
+++ rest-api/common/models/gate.js
@@ -1,5 +1,33 @@
 'use strict';
 
+const moment = require('moment');
+
 module.exports = function(gate) {
+
+  /*---------- define remote methods ----------*/
+  gate.details = (cb) => {
+    const details = {
+      nodeVersion: process.version,
+      appPath: process.argv[1],
+      dateAndTime: moment().format('LLL'),
+    };
+
+    cb(null, details);
+  };
+
+  /*---------- register remote methods ----------*/
+  gate.remoteMethod('details', {
+    description: 'Backend details of application',
+    returns: {
+      arg: 'details',
+      type: 'object',
+    },
+    http: {
+      path: '/details',
+      verb: 'get',
+      status: '200',
+      errorStatus: '500',
+    },
+  });
 
 };
