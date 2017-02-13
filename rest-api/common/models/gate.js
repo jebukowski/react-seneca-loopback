d802a92be91b224d4d7b1f330cfb03c2662f60d8
Eric Bukowski <john.eric.bukowski@gmail.com>
2017-02-13 10:15:37 -0700

    Add remote method `login` to `gate`

--- rest-api/common/models/gate.js
+++ rest-api/common/models/gate.js
@@ -15,6 +15,22 @@ module.exports = function(gate) {
     cb(null, details);
   };
 
+  gate.login = (req, res, cb) => {
+    const { username, password } = req.body;
+
+    if (username === 'demo' && password === 'password1') {
+      // user information that would be returned from db
+      const userInfo = {
+        id: 1,
+        username: 'demo',
+      };
+
+      res.status(200).send(userInfo);
+    } else {
+      res.sendStatus(401);
+    }
+  };
+
   /*---------- register remote methods ----------*/
   gate.remoteMethod('details', {
     description: 'Backend details of application',
@@ -27,6 +43,34 @@ module.exports = function(gate) {
       verb: 'get',
       status: '200',
       errorStatus: '500',
+    },
+  });
+
+  gate.remoteMethod('login', {
+    description: 'Login functionality',
+    accepts: [
+      {
+        arg: 'req',
+        type: 'object',
+        http: {
+          source: 'req',
+        },
+      },
+      {
+        arg: 'res',
+        type: 'object',
+        http: {
+          source: 'res',
+        },
+      },
+    ],
+    returns: {
+      arg: 'userInfo',
+      type: 'object',
+    },
+    http: {
+      path: '/login',
+      verb: 'post',
     },
   });
 
