4253ba9da92e8699cbd65485756f0aef2f11df93
Eric Bukowski <john.eric.bukowski@gmail.com>
2017-02-14 12:08:14 -0700

    Update `login` plugin to call the `rest-db-api`

--- microservice/src/plugins/login.js
+++ microservice/src/plugins/login.js
@@ -1,4 +1,8 @@
 import moment from 'moment';
+import fetch from 'isomorphic-fetch';
+
+// base path of REST DB API
+const path = 'http://0.0.0.0:3001/api/users';
 
 export default function() {
 
@@ -6,25 +10,30 @@ export default function() {
     role: 'api',
     cmd: 'login',
   }, function(msg, done) {
-    const { username, password } = msg;
-
-    let result = {
-      isCorrect: false,
-      userInfo: null,
-    };
-
-    // correct credentials
-    if (username === 'demo' && password === 'password1') {
-      result = {
-        isCorrect: true,
-        // NOTE: user information that would be returned from db
-        userInfo: {
-          id: 1,
-          username: 'demo',
-        },
+    fetch(`${path}/findOne?filter={"where":{"username":"${msg.username}"}}`, {
+      method: 'GET',
+      headers: { 'Accept': 'application/json' },
+    })
+    .then(response => response.json())
+    .then(user => {
+      let result = {
+        isCorrect: false,
       };
-    }
 
-    done(null, result);
+      // user was found in db based on given username & password is correct
+      if (!user.error && msg.password === user.password) {
+        result = {
+          isCorrect: true,
+          userInfo: {
+            id: user.id,
+            username: user.username,
+          },
+        };
+      }
+
+      return done(null, result);
+    })
+    .catch(err => done(err));
   });
+
 }
